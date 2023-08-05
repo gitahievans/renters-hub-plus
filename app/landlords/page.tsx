52308285eb6388a3059cdb5e408618d99730d221
gitahievans <gitahievance@gmail.com>
2023-08-06 00:19:20 +0300

    Create signup form

--- app/landlords/page.tsx
+++ app/landlords/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 const Landlords = () => {
     return (
-        <div className='min-h-[100dvh] min-w-full bg-slate-100 py-4 px-1 lg:px-10'>
+        <div className='min-h-[100dvh] min-w-full py-4 px-1 lg:px-10'>
             <section className='flex flex-col items-center '>
                 <h1 className='text-base font-medium'>Register as a Landlord/Property Manager</h1>
                 <SignUpForm />
